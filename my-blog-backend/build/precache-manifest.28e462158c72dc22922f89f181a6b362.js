d00ee84b4c12d1162455a41359b48e8da96d64c7
SiGnaLz <e3.141x@gmail.com>
2020-01-04 20:11:47 -0500

    Second Commit

---
+++ my-blog-backend/build/precache-manifest.28e462158c72dc22922f89f181a6b362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1c35b1ca651d01d1e68a7e0b63d8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1a6eb209bca640736e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e16ae4811eac6156de0a",
+    "url": "/static/js/2.52089b90.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.52089b90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb1a6eb209bca640736e",
+    "url": "/static/js/main.8247c94b.chunk.js"
+  },
+  {
+    "revision": "d32c7979582042a389ba",
+    "url": "/static/js/runtime-main.40e67f72.js"
+  }
+]);
